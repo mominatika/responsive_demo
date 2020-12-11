5f46798c5b620ed4e1ff1a87050d926634486f71
atikamomin <atikamomin97@gmail.com>
2020-12-11 09:11:32 +0530

    changes

--- js/res.js
+++ js/res.js
@@ -1,16 +1,16 @@
-function slider()
-	{
-		var menu = document.getElementById('dropdown');
-		if(menu.style.display === "none")
-		{
-			menu.style.display = "block";
+// function slider()
+// 	{
+// 		var menu = document.getElementById('dropdown');
+// 		if(menu.style.display === "none")
+// 		{
+// 			menu.style.display = "block";
 
-		}
-		else
-		{
-			menu.style.display = "none";
-		}
-	}
+// 		}
+// 		else
+// 		{
+// 			menu.style.display = "none";
+// 		}
+// 	}
 
 
 var videoplayer = document.getElementById('VideoPlayer');
